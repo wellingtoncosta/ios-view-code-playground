36c2f452a07595d611e6d81e90f247f6979f6e97
Wellington Costa <wellington.costa128@gmail.com>
2020-04-21 22:15:34 -0300

    Refactoring view code to a logical sequence of steps to be done.

--- InitialSteps/InitialSteps/MainScreen.swift
+++ InitialSteps/InitialSteps/MainScreen.swift
@@ -8,22 +8,53 @@
 
 import UIKit
 
+protocol CustomViewSteps {
+    func buildViewHierarchy()
+    func setupConstraints()
+    func setupAdditionalConfiguration()
+    func setupView()
+}
+
+extension CustomViewSteps {
+    func setupView() {
+        buildViewHierarchy()
+        setupConstraints()
+        setupAdditionalConfiguration()
+    }
+}
+
 final class MainScreen : UIView {
     
-    override init(frame: CGRect = .zero) {
-        super.init(frame: frame)
-        
-        backgroundColor = .darkGray
-        
+    lazy var button: UIButton = {
         let button = UIButton(frame: .zero)
+        
         button.backgroundColor = .red
+        
         button.setTitle("Fetch", for: .normal)
         
         // This property must be false whenever you add your own constraints, as coded below
         button.translatesAutoresizingMaskIntoConstraints = false
         
+        return button
+    }()
+    
+    override init(frame: CGRect = .zero) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension MainScreen: CustomViewSteps {
+    func buildViewHierarchy() {
         addSubview(button)
-        
+    }
+    
+    func setupConstraints() {
         // Adding a 16px margin to the left of the parent view
         button.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16).isActive = true
         
@@ -35,12 +66,11 @@ final class MainScreen : UIView {
         
         // Adding a -16px margin to the bottom of the parent view
         button.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -16).isActive = true
-        
-        
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    func setupAdditionalConfiguration() {
+        backgroundColor = .darkGray
     }
+    
     
 }
