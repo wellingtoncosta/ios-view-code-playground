18df6f1bcdab18630798dec67524a546cb406ebc
Wellington Costa <wellington.costa128@gmail.com>
2020-04-21 21:23:18 -0300

    Removing Main storyboard and displaying a blue screen.

---
+++ InitialSteps/InitialSteps/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  InitialSteps
+//
+//  Created by Wellington Pereira on 21/04/20.
+//  Copyright © 2020 Wellington Pereira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func loadView() {
+        let view = UIView(frame: UIScreen.main.bounds)
+        
+        view.backgroundColor = .blue
+        
+        self.view = view
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
